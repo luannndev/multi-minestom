a70f4ed4f4fbdd1b7cf05489163bacb5a0d3be98
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-20 17:27:18 +0200

    Add Shadow plugin and new dependencies for logging and utils
    Integrated the Shadow plugin to manage JAR packaging and specified its configuration. Added new dependencies including Log4j, SLF4J, Logback, and Minestom library, as well as Lombok for code annotations. Created a basic ServerBootstrap class to serve as the main entry point.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,40 @@
 plugins {
     id("java")
+
+    id("com.gradleup.shadow") version "8.3.0"
 }
 
 group = "dev.luan.server"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-
     compileOnly(fileTree("libs") {
         include("*.jar")
     })
+
+    implementation("net.kyori:adventure-text-minimessage:4.17.0")
+
+    // log4j
+    implementation("org.apache.logging.log4j:log4j-core:3.0.0-beta2")
+
+    implementation("org.slf4j:slf4j-api:2.1.0-alpha1")
+    implementation("ch.qos.logback:logback-classic:1.5.7")
+
+    implementation("net.minestom:minestom-snapshots:a521c4e7cd")
+
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.shadowJar {
+    archiveFileName.set("multistom-stable.jar")
+    manifest {
+        attributes["Main-Class"] = "dev.luan.server.ServerBootstrap"
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    sourceCompatibility = JavaVersion.VERSION_21.toString()
+    targetCompatibility = JavaVersion.VERSION_21.toString()
 }
---
+++ src/main/java/dev/luan/server/ServerBootstrap.java
@@ -0,0 +1,4 @@
+package dev.luan.server;
+
+public class ServerBootstrap {
+}
